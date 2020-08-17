46db5642f35d0d0d1d3df307fb290de9e3bbe5bc
jonathanmorais <jonathan.lucena@zoom.com.br>
2020-08-17 12:36:18 -0300

    teste with get openmaps sucessfull

--- routes/routes.go
+++ routes/routes.go
@@ -4,7 +4,9 @@ import (
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
+	"log"
 	"net/http"
+	"os"
 )
 
 type Info struct {
@@ -12,7 +14,48 @@ type Info struct {
 }
 
 type Coordinates struct {
-	Dates []string `json:"dates"`
+	Coord struct {
+		Lon float64 `json:"lon"`
+		Lat float64 `json:"lat"`
+	} `json:"coord"`
+	Weather []struct {
+		ID          int    `json:"id"`
+		Main        string `json:"main"`
+		Description string `json:"description"`
+		Icon        string `json:"icon"`
+	} `json:"weather"`
+	Base string `json:"base"`
+	Main struct {
+		Temp      float64 `json:"temp"`
+		FeelsLike float64 `json:"feels_like"`
+		TempMin   float64 `json:"temp_min"`
+		TempMax   float64 `json:"temp_max"`
+		Pressure  int     `json:"pressure"`
+		Humidity  int     `json:"humidity"`
+	} `json:"main"`
+	Visibility int `json:"visibility"`
+	Wind       struct {
+		Speed float64 `json:"speed"`
+		Deg   int     `json:"deg"`
+	} `json:"wind"`
+	Rain struct {
+		OneH float64 `json:"1h"`
+	} `json:"rain"`
+	Clouds struct {
+		All int `json:"all"`
+	} `json:"clouds"`
+	Dt  int `json:"dt"`
+	Sys struct {
+		Type    int    `json:"type"`
+		ID      int    `json:"id"`
+		Country string `json:"country"`
+		Sunrise int    `json:"sunrise"`
+		Sunset  int    `json:"sunset"`
+	} `json:"sys"`
+	Timezone int    `json:"timezone"`
+	ID       int    `json:"id"`
+	Name     string `json:"name"`
+	Cod      int    `json:"cod"`
 }
 
 func HomeHandler(w http.ResponseWriter, r *http.Request) {
@@ -30,7 +73,7 @@ func ReceiveInfo(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	fmt.Fprintf(w, "City: %+v", info)
+	fmt.Fprintf(w, "%+v", info)
     fmt.Print(info.City)
 
 
@@ -43,10 +86,34 @@ func ReceiveInfo(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		panic(err.Error())
 	}
-	
-	var coord Coordinates
-	json.Unmarshal(body, &coord)
-	fmt.Printf("Results: %v\n", coord)
 
+	var coord Coordinates
+
+	if err := json.Unmarshal(body, &coord); err != nil {
+		fmt.Println(err)
+		os.Exit(0)
+	}
+
+	temper := coord.Main.Temp
+	fmt.Println(temper)
+
+	var ftc float64
+
+	ftc = (temper - 273.15)
+	if err != nil {
+		log.Print(err)
+	}
+
+	fmt.Printf("%.2f", ftc)
+
+    if ftc > 30 {
+    	log.Print("Party")
+	} else if ftc > 15 && ftc < 30 {
+		log.Print("Chilli Beat")
+	} else if ftc > 10 && ftc < 24 {
+		log.Print("Rock")
+	} else if ftc <= 10 {
+		log.Print("Classical Music")
+	}
 
 }
