2a4b393ec780bfe2df7bd4bceb2e177ada5ad444
jonathanmorais <jonathan.lucena@zoom.com.br>
2020-09-02 12:11:48 -0300

    separando as functions

--- routes/routes.go
+++ routes/routes.go
@@ -58,12 +58,19 @@ type Coordinates struct {
 	Cod      int    `json:"cod"`
 }
 
-type Track struct {
+type Chilli struct {
 	Items []struct {
 		Track struct {
 			Name string `json:"name"`
 		} `json:"track"`
 	} `json:"items"`
+}
+
+type Playlist struct {
+	PartyId     string `json:"partyid"`
+	ChilliId    string `json:"chilliid"`
+	RockId      string `json:"rockid"`
+	ClassicalId string `json:"classicalid"`
 }
 
 func HomeHandler(w http.ResponseWriter, r *http.Request) {
@@ -72,18 +79,29 @@ func HomeHandler(w http.ResponseWriter, r *http.Request) {
 
 }
 
-func ReceiveInfo(w http.ResponseWriter, r *http.Request) {
+func newPlaylist(PartyId, ChilliId, RockId, ClassicalId string ) *Playlist {
+	p := Playlist{
+		PartyId:     "37i9dQZF1DX8mBRYewE6or",
+		ChilliId:    "2rN3mSrzUcgjlj1TcEDTX7",
+		RockId:      "37i9dQZF1DX8mBRYewE6or",
+		ClassicalId: "37i9dQZF1DWWEJlAGA9gs0",
+	}
+	log.Printf(p.PartyId)
+
+	return newPlaylist(PartyId, ChilliId, RockId, ClassicalId)
+}
+
+func ReceiveCity(w http.ResponseWriter, r *http.Request) float64 {
 	var info Info
 
 	err := json.NewDecoder(r.Body).Decode(&info)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusBadRequest)
-		return
+		return 0
 	}
 
 	fmt.Fprintf(w, "%+v", info)
-    fmt.Print("%s\n", info.City)
-
+	fmt.Print("%s\n", info.City)
 
 	resp, err := http.Get("http://api.openweathermap.org/data/2.5/weather?q=" + info.City + "&appid=b77e07f479efe92156376a8b07640ced")
 	if err != nil {
@@ -113,26 +131,38 @@ func ReceiveInfo(w http.ResponseWriter, r *http.Request) {
 
 	log.Printf("%.2f", ftc)
 
-	req, err := http.NewRequest("GET", "https://api.spotify.com/v1/playlists/2rN3mSrzUcgjlj1TcEDTX7/tracks?market=ES&fields=items(track(name(name)))&limit=1",nil)
-	if err != nil {log.Fatal(err)}
+	return ftc
 
-	req.Header.Set("Authorization", "Bearer BQDHNMrIAJIH5StMvZoyIOpdB8X5liY4d2m_zKepBRmf_r7_9pdmyxKAg2fXHdT3tQ_MqNVCmUl6ckz8ckGDPlDoNabpP2i-xMU8Zt-NrnfLTtx0ZbjzjefASorrbukne9miAW-qLhDA058soiF6aYQlUCDWV1qgAryrVALnzC5ci7DQmntONbY9w1s48UhlsA-XX7R-24dbrg")
+}
+
+func GetTrack(w http.ResponseWriter, r *http.Request) {
+
+	req, err := http.NewRequest("GET", "https://api.spotify.com/v1/playlists/2rN3mSrzUcgjlj1TcEDTX7/tracks?market=ES&fields=items(track(name(name)))&limit=1", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	req.Header.Set("Authorization", "Bearer BQBAP8qKdNDZeQc_vMf9iIqV-Mh68J6_DuH_WCf26rg-kAc78YosL0_W6z8HhtGnXS-JbcXwNb6AXz3sFMckX_4tawesbyb-9yz66y9N1uJDJDyi6WfWgKWLmlq9JuULrhpe6FoRQMn_RUfvoygpxaaajBdnBN3dCDoE9e6ZKs9X6BbAHIj0URFzwSpTUfHaKlw15J3yXVmoeA")
 	resp, err = http.DefaultClient.Do(req)
-	if err != nil {log.Fatal(err)}
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	body, err = ioutil.ReadAll(resp.Body)
 	if err != nil {
 		panic(err.Error())
 	}
 
-	var track Track
+	var track Chilli
 
 	err = json.Unmarshal(body, &track)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
+}
 
+func SuggestionTrack() {
 	if ftc > 30 {
     	log.Print("Party")
 		for _, val := range track.Items {
@@ -142,8 +172,8 @@ func ReceiveInfo(w http.ResponseWriter, r *http.Request) {
 	} else if ftc > 15 && ftc < 30 {
 		log.Print("Chilli Beat")
 		for _, val := range track.Items {
-			party := val.Track.Name
-			fmt.Print(party)
+			chilli := val.Track.Name
+			fmt.Print(chilli)
 		}
 	} else if ftc > 10 && ftc < 24 {
 		log.Print("Rock")
@@ -151,4 +181,5 @@ func ReceiveInfo(w http.ResponseWriter, r *http.Request) {
 		log.Print("Classical Music")
 	}
 
+	return 0
 }
